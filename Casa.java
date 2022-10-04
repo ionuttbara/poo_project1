2d9081f42172a97c02f061faab9de15686f016df
joni <76656855+jbara2002@users.noreply.github.com>
2022-10-04 08:53:50 +0300

    Delete Casa.java

--- Casa.java
+++
@@ -1,7 +0,0 @@
-public class Casa{
-	// se creeaza o lista cu 7 produse, decoratiuni pentru casa.
-	private int compare;
-	// initializare preturi pentru articole
-	private int [] preturicasa = new int [] {100,200,500,30,70,20,1000};
-	
-}
